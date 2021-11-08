059587a5587196d23910a244e19f979048df197d
dongphong543 <dongphong543@gmail.com>
2021-11-08 15:20:14 +0700

    Phong-signup+forum_start

--- src/forum_screen.js
+++ src/forum_screen.js
@@ -7,38 +7,99 @@ import { UserOutlined, QuestionCircleOutlined } from "@ant-design/icons";
 import "./App.css";
 import { formatCountdown } from "antd/lib/statistic/utils";
 
+var myCurrentDate = new Date();
+
 const Forum = () => (
-  <div style={{ overflowX: "hidden" }}>
+  <div
+    style={{
+      overflowX: "hidden",
+      position: "relative",
+    }}
+  >
     <img
       src={logo}
       alt="Logo"
       style={{
-        marginTop: "30px",
+        marginTop: "29px",
         marginLeft: "50px",
-        height: "40px",
+        height: "38px",
+        position: "absolute",
       }}
     />
-
-    <img
-      src={bluebar}
-      alt="blue bar"
+    <div
       style={{
         marginTop: "30px",
+        marginLeft: "150px",
         marginRight: "-1693px",
         height: "38px",
+        position: "absolute",
       }}
-    />
-
-    <img
-      className="photo2"
-      src={redbar}
-      alt="blue bar"
+    >
+      <span
+        style={{
+          color: "white",
+          position: "absolute",
+          marginLeft: "25px",
+          marginTop: "6px",
+        }}
+      >
+        {" "}
+        Ngày <b> {myCurrentDate.getDate()} </b> tháng
+        <b> {myCurrentDate.getMonth()} </b>
+        năm <b> {myCurrentDate.getFullYear()} </b>
+      </span>
+      <img
+        src={bluebar}
+        alt="blue bar"
+        style={{
+          height: "38px",
+        }}
+      />
+    </div>
+    <div
       style={{
-        marginTop: "30px",
-        marginLeft: "-1000px",
-        height: "40px",
+        position: "relative",
+        marginTop: "29px",
       }}
-    />
+    >
+      <span
+        className="photo2"
+        style={{
+          marginLeft: "25px",
+          marginTop: "8px",
+          position: "fixed",
+          color: "white",
+          marginLeft: "-1150px",
+        }}
+      >
+        {" "}
+        Xin chào, <b> Chung Đông Phong </b>
+      </span>
+
+      <img
+        className="photo2"
+        src={redbar}
+        alt="blue bar"
+        style={{
+          height: "39px",
+          marginLeft: "-1180px",
+        }}
+      />
+    </div>
+    <div
+      style={{
+        marginTop: "25px",
+        marginLeft: "150px",
+        marginBottom: "25px",
+        fontSize: "48px",
+        fontWeight: "bold",
+        color: "#222",
+      }}
+    >
+      Diễn đàn
+    </div>
+
+    <div style={{ backgroundColor: "red" }}> Hello</div>
   </div>
 );
 
