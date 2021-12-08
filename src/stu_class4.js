5358dbd8775623a1008116c870bda3b361a4bbae
lamtran29 <lam.tran29@hcmut.edu.vn>
2021-12-08 21:05:54 +0700

    update

--- src/stu_class4.js
+++ src/stu_class4.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Content from "./Room"
 const Tea_class1 = () => {
     return (
-        <Content room="NH 2021 - 2022 - Địa lý 11 - Hoàng Ngọc T - Lớp 12A1" />
+        <Content room="NH 2021 - 2022 - Địa lý 11 - Hoàng Ngọc T - Lớp 11A1" />
     )
 }
 
